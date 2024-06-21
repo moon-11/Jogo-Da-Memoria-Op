b0cdd4eba9e14930fdbc07f09aec0003c94e50cd
moon-11 <luanamr2222@gmail.com>
2024-06-21 20:37:24 -0300

    fix: initial cards

--- src/utils/initialCards.tsx
+++ src/utils/initialCards.tsx
@@ -1,74 +1,74 @@
-import Chopper from '../assets/img/Chopper.jpg';
-import Luffy from '../assets/img/Luffy.jpg';
-import Luffy2 from '../assets/img/Luffy2.jpg';
-import Nami from '../assets/img/Nami.jpg';
-import Robin from '../assets/img/Robin.jpg';
-import Sanji from '../assets/img/Sanji.jpg';
-import Shanks from '../assets/img/Shanks.jpg';
-import Usopp from '../assets/img/Usopp.jpg';
-import Vivi from '../assets/img/Vivi.jpg';
-import Zoro from '../assets/img/Zoro.jpg';
+import Chopper from "../assets/img/Chopper.jpg";
+import Luffy from "../assets/img/Luffy.jpg";
+import Luffy2 from "../assets/img/Luffy2.jpg";
+import Nami from "../assets/img/Nami.jpg";
+import Robin from "../assets/img/robin.jpg";
+import Sanji from "../assets/img/Sanji.jpg";
+import Shanks from "../assets/img/Shanks.jpg";
+import Usopp from "../assets/img/Usopp.jpg";
+import Vivi from "../assets/img/Vivi.jpg";
+import Zoro from "../assets/img/zoro.jpg";
 
-import CardType from '../models/card';
+import CardType from "../models/card";
 
 export const initialCards: CardType[] = [
   {
     id: 1,
-    name: 'Luffy',
+    name: "Luffy",
     image: Luffy,
     matched: false,
   },
   {
     id: 2,
-    name: 'Zoro',
+    name: "Zoro",
     image: Zoro,
     matched: false,
   },
   {
     id: 3,
-    name: 'Vivi',
+    name: "Vivi",
     image: Vivi,
     matched: false,
   },
   {
     id: 4,
-    name: 'Usopp',
+    name: "Usopp",
     image: Usopp,
     matched: false,
   },
   {
     id: 5,
-    name: 'Shanks',
+    name: "Shanks",
     image: Shanks,
     matched: false,
   },
   {
     id: 6,
-    name: 'Sanji',
+    name: "Sanji",
     image: Sanji,
     matched: false,
   },
   {
     id: 7,
-    name: 'Robin',
+    name: "Robin",
     image: Robin,
     matched: false,
   },
   {
     id: 8,
-    name: 'Nami',
+    name: "Nami",
     image: Nami,
     matched: false,
   },
   {
     id: 9,
-    name: 'Luffy2',
+    name: "Luffy2",
     image: Luffy2,
     matched: false,
   },
   {
     id: 10,
-    name: 'Chopper',
+    name: "Chopper",
     image: Chopper,
     matched: false,
   },
