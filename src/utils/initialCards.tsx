bdbcc3d637ff540bce13794fbf0a71948b20353a
SkyLuke11 <sky.lucas003@gmail.com>
2024-06-21 12:01:44 -0300

    style: imageFit cover

--- src/utils/initialCards.tsx
+++ src/utils/initialCards.tsx
@@ -1,11 +1,21 @@
-import { CardType } from '../models/card';
+import Chopper from '../assets/img/Chopper.jpg';
+import Luffy from '../assets/img/Luffy.jpg';
+import Luffy2 from '../assets/img/Luffy2.jpg';
+import Nami from '../assets/img/Nami.jpg';
+import Robin from '../assets/img/Robin.jpg';
+import Sanji from '../assets/img/Sanji.jpg';
+import Shanks from '../assets/img/Shanks.jpg';
+import Usopp from '../assets/img/Usopp.jpg';
+import Vivi from '../assets/img/Vivi.jpg';
+import Zoro from '../assets/img/Zoro.jpg';
+
+import CardType from '../models/card';
 
 export const initialCards: CardType[] = [
   {
     id: 1,
     name: 'Luffy',
-    image:
-      'https://i.pinimg.com/564x/e5/d4/3c/e5d43c951054958eca930c47c28617df.jpg',
+    image: Luffy,
     matched: false,
     frontImage: '',
     backImage: '',
@@ -13,15 +23,15 @@ export const initialCards: CardType[] = [
   {
     id: 2,
     name: 'Zoro',
-    image: '../../../assets/img/Zoro.jpg',
+    image: Zoro,
     matched: false,
     frontImage: '',
     backImage: '',
   },
   {
     id: 3,
-    name: 'ViVi',
-    image: '../../../assets/img/Vivi.jpg',
+    name: 'Vivi',
+    image: Vivi,
     matched: false,
     frontImage: '',
     backImage: '',
@@ -29,7 +39,7 @@ export const initialCards: CardType[] = [
   {
     id: 4,
     name: 'Usopp',
-    image: '../../../assets/img/Usopp.jpg',
+    image: Usopp,
     matched: false,
     frontImage: '',
     backImage: '',
@@ -37,7 +47,7 @@ export const initialCards: CardType[] = [
   {
     id: 5,
     name: 'Shanks',
-    image: '../../../assets/img/Shanks.jpg',
+    image: Shanks,
     matched: false,
     frontImage: '',
     backImage: '',
@@ -45,7 +55,7 @@ export const initialCards: CardType[] = [
   {
     id: 6,
     name: 'Sanji',
-    image: '../../../assets/img/Sanji.jpg',
+    image: Sanji,
     matched: false,
     frontImage: '',
     backImage: '',
@@ -53,7 +63,7 @@ export const initialCards: CardType[] = [
   {
     id: 7,
     name: 'Robin',
-    image: '../../../assets/img/Robin.jpg',
+    image: Robin,
     matched: false,
     frontImage: '',
     backImage: '',
@@ -61,7 +71,7 @@ export const initialCards: CardType[] = [
   {
     id: 8,
     name: 'Nami',
-    image: '../../../assets/img/Nami.jpg',
+    image: Nami,
     matched: false,
     frontImage: '',
     backImage: '',
@@ -69,7 +79,7 @@ export const initialCards: CardType[] = [
   {
     id: 9,
     name: 'Luffy2',
-    image: '../../../assets/img/Luffy2.jpg',
+    image: Luffy2,
     matched: false,
     frontImage: '',
     backImage: '',
@@ -77,7 +87,7 @@ export const initialCards: CardType[] = [
   {
     id: 10,
     name: 'Chopper',
-    image: '../../../assets/img/Chopper.jpg',
+    image: Chopper,
     matched: false,
     frontImage: '',
     backImage: '',
